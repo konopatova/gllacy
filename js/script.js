3c1cd019df2c35c534242e34d830c0fde70a2216
Anastasia Konopatova <32372877+konopatova@users.noreply.github.com>
2017-12-25 00:44:42 +0300

    не получается js

--- js/script.js
+++ js/script.js
@@ -1,49 +1,29 @@
-/*var buttonSearchBg = document.querySelector('.button.search');
-var buttonEnterBg = document.querySelector('.button.enter');
-var buttonCartBg = document.querySelector('.button.cart');
-var buttonSearch = document.querySelector('#search');
-var buttonEnter = document.querySelector('#enter');
-var buttonCart = document.querySelector('#cart');
-var animateSvg = document.querySelector('.animate-svg');
-var animateButton = document.querySelector('.animate-button-bg');
+var openModal = document.querySelector('.is-active');
+var modalWindow = document.querySelector('.modal-contact');
+var modalForm = document.querySelector('form');
+var contactForm = document.querySelector('.map-contact');
+var closeModal = document.querySelector('.modal-contact-close');
 
-buttonSearchBg.onmouseover = function() {
-    this.classList.toggle ('animate-button-bg');
-    buttonSearch.classList.toggle ('animate-svg');
-};
+contactForm.addEventListener('submit',function(e){
+    e.preventDefault();
+    modalWindow.classlist.toggle('is-active');
+});
 
-buttonEnterBg.onmouseover = function() {
-    this.classList.toggle ('animate-button-bg');
-    buttonEnter.classList.toggle ('animate-svg');
-};
+closeModal.addEventListener('click',function(){
+    modalWindow.classList.toggle('is-active');
+});
 
-buttonCartBg.onmouseover = function() {
-    this.classList.toggle ('animate-button-bg');
-    buttonCart.classList.toggle ('animate-svg');
-};
+window.addEventListener('keydown', function(esc) {
+    if (esc.keyCode===27) {
+        modalWindow.classlist.toggle('is-active');
+    };
+});
 
-buttonSearchBg.onmouseout = function() {
-    this.classList.remove ('animate-button-bg');
-    buttonSearch.classList.remove ('animate-svg');
-};
-
-buttonEnterBg.onmouseout = function() {
-    this.classList.remove ('animate-button-bg');
-    buttonEnter.classList.remove ('animate-svg');
-};
-
-buttonCartBg.onmouseout = function() {
-    this.classList.remove ('animate-button-bg');
-    buttonCart.classList.remove ('animate-svg');
-};
-/*
-
-
-/********************************************/
+console.log(openModal)
 
 function initMap() {
 var adress = {lat: 59.9387942, lng: 30.3208946};
-var pin ="./img/pin.png"
+var pin ="./img/pin.png";
 var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 17,
     center: adress 
@@ -59,5 +39,6 @@ var marker = new google.maps.Marker({
         });
       }
 map.title = '';
-icon.style.borderRadius = '10px';
-icon.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+pin.style.boxShadow ='0 2px 6px rgba(0,0,0,.3)';
+
+
